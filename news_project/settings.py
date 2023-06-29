2357d621c080e4cc61dc35f69b7f9f71f81c1c38
cocksure <sanjarwer93@gmail.com>
2023-06-29 15:52:23 +0500

    last changes

--- news_project/settings.py
+++ news_project/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ["*"]
 
 # Application definition
